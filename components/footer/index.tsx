a921ea079665b30f68ee65c347ee23f4910a1e8d
geekymv <ym2011678@foxmail.com>
2026-02-22 19:19:52 +0800

    Add web site

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -19,6 +19,20 @@ export default function () {
             <div className="my-5 mr-8 flex max-w-[200px] grow basis-[100px] flex-col space-y-5 lg:mx-10 lg:mt-0">
               <p className="font-inter font-medium text-black">Other Product</p>
               <a
+                href="http://crossclimbanswer.io"
+                target="_blank"
+                className="font-inter font-light text-gray-500"
+              >
+                Crossclimb Answer Today
+              </a>
+              <a
+                href="http://pinpointanswer.io"
+                target="_blank"
+                className="font-inter font-light text-gray-500"
+              >
+                Pinpoint Answer Today
+              </a>
+              <a
                 href="https://npe4j.com"
                 target="_blank"
                 className="font-inter font-light text-gray-500"
